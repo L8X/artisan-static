1622fb5758843011921da39d57e39966b70e6e1e
L8X <djmadilyn@gmail.com>
2021-02-03 10:31:01 +0000

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,5 +1,6 @@
 @extends('_layouts.master')
 
 @section('content')
-    <h1>Welcome to the ExProHacks Reborn Blog!</h1>
+    <h1>Welcome to the ExProHacks Blog!</h1>
+<h2><a href="https://www.unregisteredhyper.cam/posts">All Posts</a></h2>
 @endsection
